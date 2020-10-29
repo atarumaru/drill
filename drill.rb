ee64101bc9b7cc9b9716f41fda0347803f7a0d7a
wataru-a-72 <now.or.never34@icloud.com>
2020-10-30 02:09:30 +0900

    FizzBuzz問題

--- drill.rb
+++ drill.rb
@@ -1,6 +1,13 @@
-num = 77
-if num%3 == 0
-  puts "buzz"
-else
-  puts "3の倍数ではありません"
+num = 0
+while num < 100 do
+  num += 1
+  if num%5 == 0 && num%3 ==0
+    puts "FizzBuzz"
+    elsif num%5 == 0
+      puts "Buzz"
+    elsif num%3 ==0
+      puts "Fizz"
+  else
+      puts num
+  end
 end
