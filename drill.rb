d3b69a41a849b42c34abddb148c12556c8b511f6
wataru-a-72 <now.or.never34@icloud.com>
2020-10-30 02:37:40 +0900

    ハッシュの扱い

--- drill.rb
+++ drill.rb
@@ -1,13 +1,16 @@
-num = 0
-while num < 100 do
-  num += 1
-  if num%5 == 0 && num%3 ==0
-    puts "FizzBuzz"
-    elsif num%5 == 0
-      puts "Buzz"
-    elsif num%3 ==0
-      puts "Fizz"
-  else
-      puts num
-  end
-end
+profiles = []
+
+profile = {}
+profile[:name] = "太郎"
+profile[:age] = "26"
+profile[:address] = "東京"
+
+profiles << profile
+
+profile = {}
+profile [:name] = "次郎"
+profile [:age] = "24"
+profile [:address] = "大阪"
+
+profiles << profile
+puts profiles
