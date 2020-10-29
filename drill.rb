535752fb7fd707daf8529f71ea0e1923c3c537ec
wataru-a-72 <now.or.never34@icloud.com>
2020-10-30 02:51:27 +0900

    ユーザー登録

--- drill.rb
+++ drill.rb
@@ -1,16 +1,43 @@
-profiles = []
+def registration(array)
+  hash = {}
+  puts "氏名を入力してください"
+  hash[:name]=gets.chomp
+  puts "年齢を入力してください"
+  hash[:age]=gets.to_i
+  puts "居住する都道府県を入力してください"
+  hash[:address]=gets.chomp
+  array << hash
+end
 
-profile = {}
-profile[:name] = "太郎"
-profile[:age] = "26"
-profile[:address] = "東京"
+def all_users(array)
+  index = 0
+  array.each do |hash| 
+    puts "-------------------------------"
+    puts "ユーザー氏名#{index} #{hash[:name]}"
+    puts "-------------------------------"
+    index += 1
+  end
+end
 
-profiles << profile
+users = []
 
-profile = {}
-profile [:name] = "次郎"
-profile [:age] = "24"
-profile [:address] = "大阪"
-
-profiles << profile
-puts profiles
+while true do
+  puts ("
+  -------------------------------
+  行う操作を選んでください\n
+  [0] ユーザーを登録する
+  [1] ユーザー氏名一覧を見る
+  [2] 終了する
+  -------------------------------
+  ")
+  num = gets.to_i
+  if num == 0
+    registration(users)
+  elsif num == 1
+    all_users(users)
+  elsif num == 2
+    exit
+  else
+    puts "無効な値です"
+  end
+end
