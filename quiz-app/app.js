efaa4e0ee296e230a24c91761c6c792aae060633
wataru-a-72 <now.or.never34@icloud.com>
2020-11-16 22:57:05 +0900

    クイズアプリ作成

--- quiz-app/app.js
+++ quiz-app/app.js
@@ -9,21 +9,36 @@ const quiz = [
     ],
     correct: 'ニンテンドーDS'
   },{
-    question: '',
-    answers:[]
-
+    question: '記念日は？',
+    answers:[
+      '2/14',
+      '8/6',
+      '1/12',
+      '2/5'
+    ],
+    correct: '2/5'
   },{
-
+    question: 'こてつの正式な犬種名は？',
+    answers: [
+      'メキシカンホワイトチワワ',
+      'ブラックホワイトタンロングコートチワワ',
+      'ナニワブラックホワイトロングチワワ',
+      'アチチウワチチモンチッチ',
+    ],
+  correct: 'ブラックホワイトタンロングコートチワワ',
   }
 ];
 
+const quizLength = quiz.length;
+let quizIndex= 0;
+let score = 0;
 const $button = document.getElementsByTagName('button');
 const buttonLength = $button.length;
 const setupQuiz = () =>{
-  document.getElementById('js-question').textContent = question;
+  document.getElementById('js-question').textContent = quiz[quizIndex].question;
   let buttonIndex = 0;
   while(buttonIndex < buttonLength){
-    $button[buttonIndex].textContent = answers[buttonIndex];
+    $button[buttonIndex].textContent = quiz[quizIndex].answers[buttonIndex];
     buttonIndex++;
   }
 }
@@ -31,10 +46,18 @@ const setupQuiz = () =>{
 setupQuiz();
 
 const clickHandler = (e) =>{
-  if(correct === e.target.textContent ){
+  if(quiz[quizIndex].correct === e.target.textContent ){
     window.alert('正解！');
+    score++;
   }else{
     window.alert('不正解！');
+  }
+  quizIndex ++;
+
+  if(quizIndex < quizLength){
+    setupQuiz();
+  }else{
+    window.alert('終了！あなたの正解数は'+ score + '/' + quizLength + 'です！')
   }
 };
 
